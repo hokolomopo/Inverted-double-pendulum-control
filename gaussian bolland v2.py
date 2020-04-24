1e973092d9b254a0476df763edcc31050495b787
hokolomopo <adrien.minne@outlook.com>
2020-04-24 18:36:47 +0200

    Working but optimised with my ass

--- gaussian bolland v2.py
+++ gaussian bolland v2.py
@@ -15,28 +15,29 @@ from torch import Tensor
 
 import matplotlib.pyplot as plt
 
-# ENV_NAME = "InvertedDoublePendulumPyBulletEnv-v0"
-ENV_NAME = "InvertedPendulumPyBulletEnv-v0"
-DIMS = 5
-# SCALE = [-0.00207544, -0.00473823,  0.99553131,  0.00118865,  0.01364543]
-# STD = [0.05083671, 0.24168294, 0.00498501, 0.09429286, 0.55792934]
-
-
-
 SCALE = None
 STD = None
 
+ENV_NAME = "InvertedDoublePendulumPyBulletEnv-v0"
+DIMS = 9
+SCALE = [ 0.00868285,  0.03400105, -0.00312787,  0.95092393, -0.01797627, -0.10439248, 0.86726532,  0.01176883,  0.12335652]
+STD = [0.11101651, 0.58301397, 0.09502404, 0.07712284, 0.29911971, 1.78995357, 0.20914456, 0.45163139, 3.08248822]
+
+ENV_NAME = "InvertedPendulumPyBulletEnv-v0"
+DIMS = 5
+SCALE = [-0.00207544, -0.00473823,  0.99553131,  0.00118865,  0.01364543]
+STD = [0.05083671, 0.24168294, 0.00498501, 0.09429286, 0.55792934]
 
 # ENV_NAME = "CartPole-v0"
 # DIMS = 4
 
 SEED = 464684
 
-MAX_EPISODES = 100
+MAX_EPISODES = 500
 BATCH_SIZE = 64
 MAX_TIMESTEPS = 200
 
-ALPHA = 0.05
+ALPHA = 0.01
 GAMMA = 0.95
 SIGMA = 1
 
@@ -70,15 +71,15 @@ class MLPPolicy(nn.Module):
 
     def update_weight(self, states, actions, rewards, cum_rew, batch_size=1):
 
-        # decay = torch.flip(torch.cumprod(torch.ones(len(rewards)) * GAMMA, dim=0), dims=(0,)) / GAMMA
-        cum_rew_sum = torch.sum(cum_rew)
+        losses = []
+        optimizer.zero_grad()
 
-
-        log_probs = torch.sum(self(states).log_prob(actions))
-
-        loss = log_probs * cum_rew_sum / batch_size
-
-        loss.backward()
+        for i in range(len(states)):
+            G = Variable(torch.Tensor([0]))
+            for s_t, a_t, r_tt in zip(states[i][::-1], actions[i][::-1], rewards[i][::-1]):
+                G = torch.Tensor([r_tt]) + GAMMA * G
+                loss = (-1.0) * G * self(torch.Tensor(s_t)).log_prob(a_t)
+                loss.backward()
         optimizer.step()
 
 if __name__ == "__main__":
@@ -93,20 +94,18 @@ if __name__ == "__main__":
 
     for i_episode in range(MAX_EPISODES):
 
-        optimizer.zero_grad()
 
         states = []
         actions = []
         rewards = []
-        cum_rewards = []
         alive_time_batch = []
 
         for b in range(BATCH_SIZE):
 
             state = env.reset()
-            rewards_batch = []
-
-            decay = 1
+            states.append([])
+            actions.append([])
+            rewards.append([])
 
             for timesteps in range(MAX_TIMESTEPS):
                 state_tensor = Tensor(state)
@@ -114,29 +113,38 @@ if __name__ == "__main__":
                 with torch.no_grad():
                     action = policy(state_tensor).sample()
 
-                states.append(state)
-                actions.append(action)
+                states[b].append(state)
+                actions[b].append(action)
 
                 state, reward, done, _ = env.step([action.item()])
                 # state, reward, done, _ = env.step(action.item())
+
+                if reward > 0:
+                    reward = 1
+
                 if done:
-                    print("Episode {} batch {} finished after {} timesteps".format(i_episode, b, timesteps+1))
 
                     alive_time_batch.append(timesteps+1)
-                    rewards.append(0)
+                    rewards[b].append(0)
                     break
 
-                rewards.append(reward * decay)
-                decay = decay * GAMMA
-            
-            rewards_batch = rewards[-timesteps-1:]
-            cum_rewards.extend(np.cumsum(rewards_batch))
+                rewards[b].append(reward)
 
-        policy.update_weight(torch.tensor(states), torch.tensor(actions), torch.tensor(rewards), torch.tensor(cum_rewards), BATCH_SIZE)
+                if timesteps == MAX_TIMESTEPS-1:
+                    alive_time_batch.append(timesteps+1)
+
+
+            
+
+        policy.update_weight(states, actions, rewards, BATCH_SIZE)
 
         alive_time.append(np.mean(alive_time_batch))
+        print("Episode {}  finished after a mean of {:.1f} timesteps and a std of {:.2f}".format(
+            i_episode, np.mean(alive_time_batch), np.std(alive_time_batch)))
 
     env.close()
 
     plt.plot(alive_time)
+    plt.xlabel("Number of batches")
+    plt.ylabel("Iteration staying alive")
     plt.show()
